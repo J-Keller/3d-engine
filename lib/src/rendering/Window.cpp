d47024b6230c28a83bf5522a4bfd27772f9ea128
Jonas Keller <jkeller96@gmx.de>
2023-12-02 23:33:00 +0100

    Add window class to rendering
    - refactor createWindow
      - Widow should not need to know about the game -> add
        registerInputCallbacks function to force the game to provide it's
        input callbacks

---
+++ lib/src/rendering/Window.cpp
@@ -0,0 +1,98 @@
+//
+// Created by Jonas on 10/2/19.
+//
+
+#include "Window.h"
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int w, int h);
+
+GLFWwindow *Window::window;
+int Window::width;
+int Window::height;
+
+void Window::createWindow(int w, int h, const std::string &title) {
+  Window::width = w;
+  Window::height = h;
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+  if (window == nullptr) {
+    std::cerr << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+
+  glViewport(0, 0, width, height);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  initGraphics();
+
+}
+
+void Window::registerInputCallbacks(GLFWkeyfun keyCallback,
+                                    GLFWmousebuttonfun mouseCallback,
+                                    GLFWcursorposfun cursorPositionCallback,
+                                    GLFWscrollfun scrollCallback) {
+  glfwSetKeyCallback(window, keyCallback);
+  glfwSetMouseButtonCallback(window, mouseCallback);
+  glfwSetCursorPosCallback(window, cursorPositionCallback);
+  glfwSetScrollCallback(window, scrollCallback);
+}
+
+void Window::clear() {
+  //TODO: Stencil Buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void Window::initGraphics() {
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+  glFrontFace(GL_CW);
+  glCullFace(GL_FRONT);
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_DEPTH_TEST);
+
+  glEnable(GL_DEPTH_CLAMP);
+
+}
+
+void Window::render() {
+  glfwSwapBuffers(window);
+  glfwPollEvents();
+}
+
+void Window::dispose() {
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
+
+bool Window::isCloseRequested() {
+  return glfwWindowShouldClose(window);
+}
+
+float Window::getAspectRatio() {
+  return (float) width / (float) height;
+}
+
+// Callback Methods
+void framebuffer_size_callback(GLFWwindow *window, int w, int h) {
+  Window::width = w;
+  Window::height = h;
+  glViewport(0, 0, w, h);
+}
---
+++ lib/src/rendering/Window.h
@@ -0,0 +1,35 @@
+//
+// Created by Jonas on 10/2/19.
+//
+
+#ifndef MCCLONE_WINDOW_H
+#define MCCLONE_WINDOW_H
+
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+class Window {
+ public:
+  static GLFWwindow *window;
+
+  static int width;
+  static int height;
+
+  static void createWindow(int w, int h, const std::string &title);
+  static void registerInputCallbacks(GLFWkeyfun keyCallback,
+                                     GLFWmousebuttonfun mouseCallback,
+                                     GLFWcursorposfun cursorPositionCallback,
+                                     GLFWscrollfun scrollCallback);
+  static void clear();
+  static void initGraphics();
+  static void render();
+  static void dispose();
+  static float getAspectRatio();
+
+  static bool isCloseRequested();
+ private:
+};
+
+#endif //MCCLONE_WINDOW_H
